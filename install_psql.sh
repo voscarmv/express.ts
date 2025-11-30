f8cbb207bff0e462675549f18df904699f522009
Oscar Mier <voscarmv@gmail.com>
2025-11-29 18:52:11 -0600

    Install psql

---
+++ install_psql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo 'Install PSQL (sudo apt install) and create new DB? (y/n) '
+read YN
+
+if test $YN == 'n' -o $YN == 'N' ; then
+    exit
+fi
+
+sudo apt install -y postgresql postgresql-contrib
+
+read -p "Postgres username: " PGUSER
+read -s -p "Postgres password: " PASS
+echo
+read -p "Database name: " DB
+
+sudo -u postgres psql <<EOF
+CREATE DATABASE $DB;
+CREATE USER $PGUSER WITH PASSWORD '$PASS';
+GRANT ALL PRIVILEGES ON DATABASE $DB TO $PGUSER;
+\c $DB
+GRANT ALL ON SCHEMA public TO $PGUSER;
+EOF
+
+echo "db_url=postgres://$PGUSER:$PASS@localhost/$DB" >> .env
+echo "✅ Database, user, and .env file created. Edit .env if needed."
